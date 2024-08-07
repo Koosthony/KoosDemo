bbb79f2d48cba509839eb820cff6d8324fc69687
KoosVeldman <135098473+Koosthony@users.noreply.github.com>
2024-08-08 00:12:59 +0200

    edited

--- GITrojectTest/src/welcome/myFirstProg.java
+++ GITrojectTest/src/welcome/myFirstProg.java
@@ -2,6 +2,15 @@ package welcome;
 
 public class myFirstProg {
 public static void main(String[] args) {
+	
+	int i = 2; 
+	int j = 3;
+	int z=i+j;
+	
+	for (int b=0; b<5; b++) {
+		System.out.println(z);
+	}
+	//System.out.println("my result : "+ z);
 	System.out.println("Hello World");
 }
 }
